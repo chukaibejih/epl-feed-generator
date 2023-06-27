bc226c911b3bb4071ec49ca04947a62df1e6f3d8
Ibejih <chukaibejih@gmail.com>
2023-06-27 08:26:08 +0100

    data filter logic

--- server/data_filter.py
+++ server/data_filter.py
@@ -34,32 +34,41 @@ epl_key_names = [
 ]
 
 
-def operations_callback(ops, record):
+def operations_callback(ops):
     # Filter posts with epl keywords
     # After our feed alg, save posts into the DB
     # Also, we should process deleted posts to remove them from our DB and keep it in sync
 
-    posts_to_create = [
-        {
-            'uri': created_post['uri'],
-            'cid': created_post['cid'],
-            'reply_parent': record.reply.parent.uri if record.reply else None,
-            'reply_root': record.reply.root.uri if record.reply else None,
-        }
-        for created_post in ops['posts']['created']
-        if any(key.lower() in record['text'] for key in epl_key_names)
-    ]
+    posts_to_create = []
+    for created_post in ops['posts']['created']:
+        record = created_post['record']
+
+        # #epl posts
+        if any(key in record['text'].lower() for key in epl_key_names):
+            reply_parent = None
+            if record.reply and record.reply.parent.uri:
+                reply_parent = record.reply.parent.uri
+
+            reply_root = None
+            if record.reply and record.reply.root.uri:
+                reply_root = record.reply.root.uri
+
+            post_dict = {
+                'uri': created_post['uri'],
+                'cid': created_post['cid'],
+                'reply_parent': reply_parent,
+                'reply_root': reply_root,
+            }
+            posts_to_create.append(post_dict)
 
     posts_to_delete = [p['uri'] for p in ops['posts']['deleted']]
     if posts_to_delete:
-        with db.atomic():
-            Post.delete().where(Post.uri.in_(posts_to_delete))
-            logger.info(f'Deleted from feed: {len(posts_to_delete)}')
+        Post.delete().where(Post.uri.in_(posts_to_delete))
+        logger.info(f'Deleted from feed: {len(posts_to_delete)}')
 
     if posts_to_create:
         with db.atomic():
             for post_dict in posts_to_create:
                 Post.create(**post_dict)
         logger.info(f'Added to feed: {len(posts_to_create)}')
-
 
